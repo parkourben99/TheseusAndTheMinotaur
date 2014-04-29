3675640ac6e6bff667f52bbaedcf904a7a5695bb
parkourben99 <ben@ayles.co.nz>
2014-04-30 10:51:35 +1200

    game window size set

--- WindowsFormsApplication4/NewGame.Designer.cs
+++ WindowsFormsApplication4/NewGame.Designer.cs
@@ -41,19 +41,23 @@
             this.lblLevelName = new System.Windows.Forms.Label();
             this.lblScore = new System.Windows.Forms.Label();
             this.lblUserName = new System.Windows.Forms.Label();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.bntMenu = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblMoves = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ltbLevel
             // 
             this.ltbLevel.FormattingEnabled = true;
-            this.ltbLevel.Location = new System.Drawing.Point(612, 32);
+            this.ltbLevel.Location = new System.Drawing.Point(829, 32);
             this.ltbLevel.Name = "ltbLevel";
             this.ltbLevel.Size = new System.Drawing.Size(160, 212);
             this.ltbLevel.TabIndex = 0;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(612, 279);
+            this.btnSave.Location = new System.Drawing.Point(829, 279);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(160, 23);
             this.btnSave.TabIndex = 1;
@@ -62,7 +66,7 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(612, 250);
+            this.btnLoad.Location = new System.Drawing.Point(829, 250);
             this.btnLoad.Name = "btnLoad";
             this.btnLoad.Size = new System.Drawing.Size(160, 23);
             this.btnLoad.TabIndex = 2;
@@ -74,13 +78,13 @@
             this.pnlGame.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pnlGame.Location = new System.Drawing.Point(12, 32);
             this.pnlGame.Name = "pnlGame";
-            this.pnlGame.Size = new System.Drawing.Size(582, 499);
+            this.pnlGame.Size = new System.Drawing.Size(700, 700);
             this.pnlGame.TabIndex = 3;
             this.pnlGame.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlGame_Paint);
             // 
             // btnLeft
             // 
-            this.btnLeft.Location = new System.Drawing.Point(616, 377);
+            this.btnLeft.Location = new System.Drawing.Point(833, 458);
             this.btnLeft.Name = "btnLeft";
             this.btnLeft.Size = new System.Drawing.Size(75, 37);
             this.btnLeft.TabIndex = 4;
@@ -89,7 +93,7 @@
             // 
             // btnUp
             // 
-            this.btnUp.Location = new System.Drawing.Point(655, 334);
+            this.btnUp.Location = new System.Drawing.Point(872, 415);
             this.btnUp.Name = "btnUp";
             this.btnUp.Size = new System.Drawing.Size(75, 37);
             this.btnUp.TabIndex = 5;
@@ -98,7 +102,7 @@
             // 
             // btnRight
             // 
-            this.btnRight.Location = new System.Drawing.Point(697, 377);
+            this.btnRight.Location = new System.Drawing.Point(914, 458);
             this.btnRight.Name = "btnRight";
             this.btnRight.Size = new System.Drawing.Size(75, 37);
             this.btnRight.TabIndex = 6;
@@ -107,7 +111,7 @@
             // 
             // btnDown
             // 
-            this.btnDown.Location = new System.Drawing.Point(655, 420);
+            this.btnDown.Location = new System.Drawing.Point(872, 501);
             this.btnDown.Name = "btnDown";
             this.btnDown.Size = new System.Drawing.Size(75, 37);
             this.btnDown.TabIndex = 7;
@@ -116,9 +120,9 @@
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(697, 527);
+            this.btnExit.Location = new System.Drawing.Point(828, 775);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.Size = new System.Drawing.Size(161, 23);
             this.btnExit.TabIndex = 8;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -126,9 +130,9 @@
             // 
             // btnHelp
             // 
-            this.btnHelp.Location = new System.Drawing.Point(616, 527);
+            this.btnHelp.Location = new System.Drawing.Point(829, 746);
             this.btnHelp.Name = "btnHelp";
-            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.Size = new System.Drawing.Size(160, 23);
             this.btnHelp.TabIndex = 9;
             this.btnHelp.Text = "Help";
             this.btnHelp.UseVisualStyleBackColor = true;
@@ -137,7 +141,7 @@
             // lblLevelName
             // 
             this.lblLevelName.AutoSize = true;
-            this.lblLevelName.Location = new System.Drawing.Point(608, 9);
+            this.lblLevelName.Location = new System.Drawing.Point(825, 9);
             this.lblLevelName.Name = "lblLevelName";
             this.lblLevelName.Size = new System.Drawing.Size(71, 13);
             this.lblLevelName.TabIndex = 11;
@@ -161,11 +165,51 @@
             this.lblUserName.TabIndex = 13;
             this.lblUserName.Text = "lblUserName";
             // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(828, 308);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(160, 23);
+            this.btnUndo.TabIndex = 14;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            // 
+            // bntMenu
+            // 
+            this.bntMenu.Location = new System.Drawing.Point(828, 717);
+            this.bntMenu.Name = "bntMenu";
+            this.bntMenu.Size = new System.Drawing.Size(160, 23);
+            this.bntMenu.TabIndex = 15;
+            this.bntMenu.Text = "Menu";
+            this.bntMenu.UseVisualStyleBackColor = true;
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(579, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(40, 13);
+            this.lblTime.TabIndex = 16;
+            this.lblTime.Text = "lblTime";
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Location = new System.Drawing.Point(711, 9);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(49, 13);
+            this.lblMoves.TabIndex = 17;
+            this.lblMoves.Text = "lblMoves";
+            // 
             // NewGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.ClientSize = new System.Drawing.Size(1004, 812);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.bntMenu);
+            this.Controls.Add(this.btnUndo);
             this.Controls.Add(this.lblUserName);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.lblLevelName);
@@ -180,8 +224,8 @@
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.ltbLevel);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(800, 600);
-            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.MaximumSize = new System.Drawing.Size(1020, 850);
+            this.MinimumSize = new System.Drawing.Size(1020, 850);
             this.Name = "NewGame";
             this.Text = "New Game";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -205,6 +249,10 @@
         private System.Windows.Forms.Label lblLevelName;
         private System.Windows.Forms.Label lblScore;
         private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button bntMenu;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblMoves;
 
     }
 }
