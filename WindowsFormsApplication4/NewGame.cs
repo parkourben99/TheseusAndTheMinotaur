94737589c679bb3d6d9043ff6338d23d86d42dac
parkourben99 <ben@ayles.co.nz>
2014-04-30 20:39:17 +1200

    created the images and set the functions

--- WindowsFormsApplication4/NewGame.cs
+++ WindowsFormsApplication4/NewGame.cs
@@ -42,33 +42,156 @@ namespace WindowsFormsApplication4
         private void btnLeft_Click(object sender, EventArgs e)
         {
 
-            
-
-            using (Graphics g = pnlGame.CreateGraphics())
-            {
-                string wallHorizontalImg = @"C:\Users\Benjamin Ayles\Desktop\Project\TheseusAndTheMinotaur\WindowsFormsApplication4\Images\Walls\horizontal.png";
-                Image wallHorizontal = Image.FromFile(wallHorizontalImg);
-                g.DrawImage(wallHorizontal, new Point(0,0));
-            }
-
-
 
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
 
-            string levelTest = "233311244113441144413333";
+            string wallTest = "233311244113441144413333";
 
-            buildLevel(levelTest);
+            string thesusTest = "21";
+
+            string minotaurTest = "41";
+
+            string exitTest = "55";
+
+            buildLevel(wallTest, thesusTest, minotaurTest, exitTest);
+
         }
 
 
-        protected void bulidLevel(string levelName)
+
+        protected void buildLevel(string wallCreate, string thesusCreate, string minotaurCreate, string exitCreate)
         {
 
-           //write the method to where to wall is placed
+            // change the string to a char array
+            char[] wallArray = wallCreate.ToCharArray();
+            
+            // change the sting to a char array
+            char[] thesusArray = thesusCreate.ToCharArray();
+
+            // change the sting to a char array
+            char[] minotaurArray = minotaurCreate.ToCharArray();
+
+            // change the sting to a char array
+            char[] exitArray = exitCreate.ToCharArray();
+
+            // fining the grid size
+            int gridSize = calcGridSize(wallCreate.Length);
+
+            //build the map from the array
+            buildMap(wallArray, gridSize);
+
+            //place Thesus & Minotaur & Exit
+            buildCharacters(thesusArray, minotaurCreate, exitArray);
+
+
+            lblScore.Text = wallArray[1].ToString();
 
         }
+
+        protected void buildMap(char[] wallArray, int gridSize)
+        {
+            //change for diffrent computer -wip-
+            string begingPath = @"C:\Users\Benjamin Ayles\Desktop\Project\TheseusAndTheMinotaur\WindowsFormsApplication4\";
+            
+            //set the images
+            string wallHorizontalImg = begingPath + @"Images\Walls\horizontal.png";
+            Image wallHorizontal = Image.FromFile(wallHorizontalImg);
+
+            string wallVerticalImg = begingPath + @"Images\Walls\vertical.png";
+            Image wallVertical = Image.FromFile(wallVerticalImg);
+
+            string wallTopLeftImg = begingPath + @"Images\Walls\top_left.png";
+            Image wallTopLeft = Image.FromFile(wallTopLeftImg);
+
+            string wallTopRightImg = begingPath + @"Images\Walls\top_right.png";
+            Image wallTopRight = Image.FromFile(wallTopRightImg);
+
+            string wallBottomLeftImg = begingPath + @"Images\Walls\bottom_left.png";
+            Image wallBottomLeft = Image.FromFile(wallBottomLeftImg);
+
+            string wallBottomRightImg = begingPath + @"Images\Walls\bottom_right.png";
+            Image wallBottomRight = Image.FromFile(wallBottomRightImg);
+
+
+
+            using (Graphics graphics = pnlGame.CreateGraphics())
+            {
+                graphics.DrawImage(wallHorizontal, new Point(0, 0));
+            }
+
+        }
+
+
+        protected int calcGridSize(int wallLength)
+        {
+            int gridSize = 0;
+
+            if (wallLength >= 9)
+            {
+                // set the grid size if 3x3
+                gridSize = 9;
+
+                if (wallLength >= 16)
+                {
+                    // set gird size if 4x4
+                    gridSize = 16;
+
+                    if (wallLength >= 25)
+                    {
+                        //set grid size if 5x5
+                        gridSize = 25;
+
+                        if (wallLength >= 36)
+                        {
+                            //set grid size if 6x6
+                            gridSize = 36;
+
+                            if (wallLength >= 49)
+                            {
+                                //set grid size if 7x7
+                                gridSize = 49;
+
+                                if (wallLength >= 64)
+                                {
+                                    //set grid size if 8x8
+                                    gridSize = 64;
+
+                                    if (wallLength >= 81)
+                                    {
+                                        //set grid size if 9x9
+                                        gridSize = 81;
+
+                                        if (wallLength >= 100)
+                                        {
+                                            //set grid size if 10x10
+                                            gridSize = 100;
+
+                                        }
+
+                                    }
+
+                                }
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+
+            return gridSize;
+        }
+
+
+
+
+
+
+
+
     }
 }
