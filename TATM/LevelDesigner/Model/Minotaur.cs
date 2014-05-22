79831178091348df9555eb54d2d88c04e6e05068
TheBice <TheBice@hotmail.com>
2014-05-23 10:47:39 +1200

    Comment added

--- TATM/LevelDesigner/Model/Minotaur.cs
+++ TATM/LevelDesigner/Model/Minotaur.cs
@@ -9,33 +9,42 @@ namespace LevelDesign.Model
 {
     public class Minotaur : Character
     {
+        //Image of Minotaur
         private Image _MinotaurImage;
 
         public Image MinotaurImage
         {
+            //Get/Set the image of Minotaur
             get { return _MinotaurImage; }
             set { _MinotaurImage = value; }
         }
+
+        //The Minotaur's position of x
         private int _XPosition;
 
         public int XPosition
         {
+            //Get/Set Minotaur's x position
             get { return _XPosition; }
             set { _XPosition = value; }
         }
 
+        //The Minotaur's position of Y
         private int _YPosition;
 
         public int YPosition
         {
+            //Get/Set Minotaur's Y position
             get { return _YPosition; }
             set { _YPosition = value; }
         }
 
+        //Minotaur's move remaining
         private int _MovesRemaining;
 
         public int MovesRemaining
         {
+            //Get/Set Minotaur's remaining moves
             get { return _MovesRemaining; }
             set { _MovesRemaining = value; }
         }
--- TATM/LevelDesigner/Model/Theseus.cs
+++ TATM/LevelDesigner/Model/Theseus.cs
@@ -9,33 +9,42 @@ namespace LevelDesign.Model
 {
     public class Theseus : Character
     {
+        //Image of Theseus
         private Image _TheseusImage;
-
+                
         public Image TheseusImage
         {
+            //Get/Set the image of Theseus
             get { return _TheseusImage; }
             set { _TheseusImage = value; }
         }
+
+        //The Theseus's position of x
         private int _XPosition;
 
         public int XPosition
         {
+            //Get/Set Theseus's x position
             get { return _XPosition; }
             set { _XPosition = value; }
         }
 
+        //The Theseus's position of y
         private int _YPosition;
 
         public int YPosition
         {
+            //Get/Set Theseus's y position
             get { return _YPosition; }
             set { _YPosition = value; }
         }
 
+        //Remaining moves for Theseus
         private int _MovesRemaining;
 
         public int MovesRemaining
         {
+            //Get/Set Theseus's remaining move
             get { return _MovesRemaining; }
             set { _MovesRemaining = value; }
         }
